ad5fcea0dc67277948ceb488808eaaa18c032da8
Joshua Beatty <joshuatb6@gmail.com>
2025-07-02 08:11:22 -0600

    changing to spawn

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { exec, spawn } from "child_process";
+import { spawn } from "child_process";
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -7,14 +7,33 @@ const app = express();
 app.use(express.static("dist"));
 
 app.get("/api/info", (req, res) => {
-  const child = exec("sh -c 'ncdu /drive -o -'", (err, stdout, stderr) => {
-    res.send({err, stdout, stderr})
-  });
+    // sh -c "ncdu /drive -o -"
+  const child = spawn("sh", ["-c", "ncdu /drive -o - | tr -d '\\n\\r'"]);
   req.on("close", () => {
     child.kill();
+  });
+
+  child.stdout.on("data", (data) => {
+    res.write(data)
+  });
+
+  child.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  child.on("close", (code) => {
+    res.end();
   });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+//   const child = exec("sh -c 'ncdu /drive -o -'", (err, stdout, stderr) => {
+//     res.send({err, stdout, stderr})
+//   });
+//   req.on("close", () => {
+//     child.kill();
+//   });
