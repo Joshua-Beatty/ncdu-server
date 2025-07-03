de54c3f7780b3ddf03586655087cab00b2314280
Joshua Beatty <joshuatb6@gmail.com>
2025-07-03 13:15:22 -0600

    starting scan on page load

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "./components/ui/button";
 import { Card } from "./components/ui/card";
 import useStartScan from "./utils/useStartScan";
@@ -7,24 +7,14 @@ import DirectoryBrowser from "./DirectoryBrowser";
 // import viteLogo from '/vite.svg'
 
 function App() {
-  const [started, setStarted] = useState(false);
   const { data, err, start, isDone } = useStartScan();
+  useEffect(()=>{start()}, [])
   return (
     <>
       {!isDone && (
         <div className="flex justify-center items-center w-full h-screen p-4">
           <Card className="flex flex-col justify-center items-center min-w-6/12 min-h-3/12 p-4">
-            {!started && (
-              <Button
-                onClick={() => {
-                  setStarted(true);
-                  start();
-                }}
-              >
-                Start Drive Scan
-              </Button>
-            )}
-            {started && !isDone && <pre className="w-full">{data}</pre>}
+            {!isDone && <pre className="w-full">{data}</pre>}
             {err && JSON.stringify(err)}
           </Card>
         </div>
